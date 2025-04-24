48c1fbbe3e6a9d5502b3303320cecd8902d79b63
Dhaval Patel <rnwdhaval1@gmail.com>
2025-04-24 19:31:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose')
+const routes = require('./Routes/Routes')
+require('dotenv').config(); // Load environment variables from .env file if it exists most important thing to do
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// mongodb://localhost:27017/jwt
+mongoose.connect('mongodb://127.0.0.1:27017/jwt')
+    .then(() => console.log('Data base connected'))
+    .catch((error) => console.log(error))
+
+
+// Middleware to log request details
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/api', routes)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
